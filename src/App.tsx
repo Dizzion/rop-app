2d02a059992f0cc7e3156cbc26d0c03a78ce3910
Dizzion <gofrodude_197@hotmail.com>
2021-05-23 00:36:16 -0700

    added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './Components/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <h1>ROP App</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.scss";
+
+function Header() {
+    return (
+        <div>
+            <h1>ROP App</h1>
+        </div>
+    );
+}
+
+export default Header;
