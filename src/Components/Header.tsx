feff49f690ddbdbf40fa23335e1e7a7225781d08
Dizzion <gofrodude_197@hotmail.com>
2021-05-23 01:26:38 -0700

    added styles for header and changed background based on alliance input

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,8 +3,8 @@ import "./Header.scss";
 
 function Header() {
     return (
-        <div>
-            <h1>ROP App</h1>
+        <div className="header">
+            <h1>Reign of Phoenix</h1>
         </div>
     );
 }
