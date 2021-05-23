209982637cd162acadf98ec993d7730711681de5
Dizzion <gofrodude_197@hotmail.com>
2021-05-23 01:08:30 -0700

    fixed bootstrap issue and created cards for player names

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from './Components/Header';
+import Card from './Components/TeamCards/Card';
 import './App.scss';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Container fluid>
+        <Row>
+          <Col sm><Card/></Col>
+          <Col sm><Card/></Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/Components/TeamCards/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function Card() {
+    return (
+        <div>
+            <ListGroup>
+                <ListGroup.Item>Person 1</ListGroup.Item>
+                <ListGroup.Item>Person 2</ListGroup.Item>
+                <ListGroup.Item>Person 3</ListGroup.Item>
+                <ListGroup.Item>Person 4</ListGroup.Item>
+                <ListGroup.Item>Person 5</ListGroup.Item>
+                <ListGroup.Item>Person 6</ListGroup.Item>
+            </ListGroup>
+        </div>
+    );
+}
+
+export default Card;
