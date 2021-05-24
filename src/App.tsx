4c1a9fd694a603bfbdb0c9382e11d29e7c3d774a
Dizzion <gofrodude_197@hotmail.com>
2021-05-24 09:00:41 -0700

    added start of form to add members to state

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import Header from './Components/Header';
 import Card from './Components/TeamCards/Card';
 import './App.scss';
-import { Button, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { Button, Col, Container, InputGroup, FormControl, Row } from 'react-bootstrap';
 
 const App: React.FC = () => {
   const [members, setMembers] = useState<{id: string, troopTier: number, marchSize: number, rallySize: number, fighters: {attack: number, defense: number, health: number}, shooters: {attack: number, defense: number, health: number}, rider: {attack: number, defense: number, health: number}}[]>([]);
 
-  const memberAddHandler = (member: object) => {
-    setMembers([])
+  const memberAddHandler = (member: {id: string, troopTier: number, marchSize: number, rallySize: number, fighters: {attack: number, defense: number, health: number}, shooters: {attack: number, defense: number, health: number}, rider: {attack: number, defense: number, health: number}}) => {
+    setMembers([member])
   };
 
   return (
@@ -16,16 +16,33 @@ const App: React.FC = () => {
       <Header/>
       <div>
         <h2>Add Member</h2>
-        <ListGroup>
-          <ListGroup.Item>Game ID: </ListGroup.Item>
-          <ListGroup.Item>Troop Tier: </ListGroup.Item>
-          <ListGroup.Item>March Size: </ListGroup.Item>
-          <ListGroup.Item>Rally Size: </ListGroup.Item>
-          <ListGroup.Item>Fighter Stats: Attack:   Defense:  Health:  </ListGroup.Item>
-          <ListGroup.Item>Shooter Stats: Attack:   Defense:  Health:  </ListGroup.Item>
-          <ListGroup.Item>Rider Stats: Attack:   Defense:  Health:  </ListGroup.Item>
-        </ListGroup>
-        <Button>Sort Members</Button>
+        <Container fluid="sm">
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text id="Username">Username</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl placeholder="In Game Name" aria-label="id" aria-describedby="Username"/>
+          
+            <InputGroup.Prepend>
+              <InputGroup.Text id="tier">Troop Tier</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl placeholder="5,6,7,8,9,10" aria-label="troopTier" aria-describedby="teir"/>
+          
+            <InputGroup.Prepend>
+              <InputGroup.Text id="march">March Size</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl placeholder="130000" aria-label="marchSize" aria-describedby="march"/>
+          
+            <InputGroup.Prepend>
+              <InputGroup.Text id="rally">Rally Size</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl placeholder="500000" aria-label="rallySize" aria-describedby="rally"/>
+          </InputGroup>
+          <InputGroup>Fighter Stats: Attack:   Defense:  Health:  </InputGroup>
+          <InputGroup>Shooter Stats: Attack:   Defense:  Health:  </InputGroup>
+          <InputGroup>Rider Stats: Attack:   Defense:  Health:  </InputGroup>
+        </Container>
+        <Button>Add Members</Button>
       </div>
       <Container fluid>
         <Row>
