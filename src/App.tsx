e7cd3bc86bec142ef4159336d3bddb9e0aba4236
Dizzion <gofrodude_197@hotmail.com>
2021-05-26 09:05:53 -0700

    started work on getting a list of members to populate at the top of page as list fills up

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,23 @@ import { Player } from "./Model/player.model";
 import NewMember from "./Components/NewMember";
 import CardContainer from './Components/CardContainer';
 import "./App.scss";
+import { ListGroup } from "react-bootstrap";
 
 const App: React.FC = () => {
   const [members, setMembers] = useState<Player[]>([]);
 
   const memberAddHandler = (member: Player) => {
-    setMembers([member]);
+    setMembers(prevMembers => [...prevMembers, member]);
   };
+
+  let memberList = members.map((per, i) => {
+    return <ListGroup.Item>{per.username}</ListGroup.Item>
+  });
 
   return (
     <div className="App">
       <Header />
+      {members ? memberList: ''}
       <NewMember onAddMember={memberAddHandler}/>
       <CardContainer Members={members}/>
     </div>
