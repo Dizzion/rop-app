05fbf82953ad758602f6e137b0ecccfd2c8fe4ab
Dizzion <gofrodude_197@hotmail.com>
2021-05-28 06:37:35 -0700

    finished creation function for groups

--- src/Components/CardContainer.tsx
+++ src/Components/CardContainer.tsx
@@ -5,15 +5,23 @@ import { Player } from "../Model/player.model";
 
 const CardContainer: React.FC<any> = props => {
 
+    let groups: Player[][] = [];
+
     const createGroups = () => {
-        let numGroups: number = props.members.length / 6
+        let numGroups: number = Math.ceil(props.members.length / 6);
+        groups = new Array(numGroups)
+            .fill('')
+            .map((_, i) => props.members.slice(i * 6, (i + 1) * 6));
     }
 
   return (
     <div>
       {/*add sorting and use algorythm to sort all the members when added to site
                 and when pulled from DB*/}
-      <Button variant="outline-success" size="lg">
+      <Button 
+        variant="outline-success" 
+        size="lg"
+        onClick={createGroups}>
         Sort Into Groups
       </Button>
       <Container fluid>
