1d450347685aee5cd10b796f1b95834e15e8d405
Dizzion <gofrodude_197@hotmail.com>
2021-05-30 02:23:43 -0700

    formating

--- src/Components/CardContainer.tsx
+++ src/Components/CardContainer.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Container, Row, Button } from "react-bootstrap";
 
-const CardContainer: React.FC<any> = props => {
-
+const CardContainer: React.FC<any> = (props) => {
   return (
-      <>
-      <Button variant="outline-info" size="lg" onClick={props.createGroups} className="sortButton">
+    <>
+      <Button
+        variant="outline-info"
+        size="lg"
+        onClick={props.createGroups}
+        className="sortButton"
+      >
         Sort Into Groups
       </Button>
       <Container fluid>
-        <Row>{props.cards ? props.cards : ''}</Row>
+        <Row>{props.cards ? props.cards : ""}</Row>
       </Container>
-      </>
+    </>
   );
 };
 
--- src/Components/TeamCards/TCard.tsx
+++ src/Components/TeamCards/TCard.tsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
 
-
-const TCard: React.FC<any> = props => {
-    return (
-        <div>
-            <ListGroup>
-                <ListGroup.Item active>Group {props.num + 1}</ListGroup.Item>
-                <ListGroup.Item>{props.teams[0].username}</ListGroup.Item>
-                <ListGroup.Item>{props.teams[1] ? props.teams[1].username : ''}</ListGroup.Item>
-                <ListGroup.Item>{props.teams[2] ? props.teams[2].username : ''}</ListGroup.Item>
-                <ListGroup.Item>{props.teams[3] ? props.teams[3].username : ''}</ListGroup.Item>
-                <ListGroup.Item>{props.teams[4] ? props.teams[4].username : ''}</ListGroup.Item>
-                <ListGroup.Item>{props.teams[5] ? props.teams[5].username : ''}</ListGroup.Item>
-            </ListGroup>
-        </div>
-    );
-}
+const TCard: React.FC<any> = (props) => {
+  return (
+    <div>
+      <ListGroup>
+        <ListGroup.Item active>Group {props.num + 1}</ListGroup.Item>
+        <ListGroup.Item>{props.teams[0].username}</ListGroup.Item>
+        <ListGroup.Item>
+          {props.teams[1] ? props.teams[1].username : ""}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {props.teams[2] ? props.teams[2].username : ""}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {props.teams[3] ? props.teams[3].username : ""}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {props.teams[4] ? props.teams[4].username : ""}
+        </ListGroup.Item>
+        <ListGroup.Item>
+          {props.teams[5] ? props.teams[5].username : ""}
+        </ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
+};
 
 export default TCard;
